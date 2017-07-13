0e15b5a91d62c89d0811b87a6fb180c90b9529d3
madison kanna <madisonkanna@gmail.com>
2017-07-12 20:27:34 -0700

    Add new BooksList Component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import React from 'react'
+import React, { Component } from 'react'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
+import './'
 
-class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
+class BooksApp extends Component {
+  state = { //use URL in browser's address bar
+
+
     showSearchPage: true
   }
 
