3de87d57eb894290bc3eec4e316a38d84bc722a3
madison kanna <madisonkanna@gmail.com>
2017-07-13 11:34:05 -0700

    Initialize state

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -3,13 +3,19 @@ import React, { Component } from 'react'
 //Create a searchbar
 
 class SearchBar extends Component {
-
+	constructor(props) {
+		super(props);
+		this.state = { term: ''};
+	}
 
 
 	render() {
-		return <input type="text" placeholder="Search by title or author"/>
+		return <input onChange={event => console.log(event.target.value)} />;
 	}
 
+	onInputChange(event) { 
+		console.log(event);
+	}
 }
 
 export default SearchBar;
