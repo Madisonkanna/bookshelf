3438e756a44b304cf2cf0bf0e6b7362c7807d29d
madison kanna <madisonkanna@gmail.com>
2017-07-20 21:59:11 -0700

    Change architecture of app

---
+++ src/ListBooks.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+//import escapeRegExp from 'escape-string-regexp'
+//import sortBy from 'sort-by'
+import * as BooksAPI from './BooksAPI'
+
+//Create a searchbar component
+
+//TO DO: Bookshelf component which will contains Book components and map the books I retrieve with the `getAll()`
+
+//  console.log(BooksAPI.search('Make'));
+// Above, this is how you search and return your array of objects with 'make' in the subject line :)
+
+class ListBooks extends Component {
+
+	//static propTypes = {
+	//	books: PropTypes.array.isRequired
+	//}
+
+	state = {
+		term: ''
+	}
+
+	handleChange = (event) => {
+		this.setState({term: event.target.value})
+	}
+
+	//searchBook = (term) => {
+	//	this.setState(BooksAPI.search);
+	//}
+
+
+	render() {
+
+		return (
+			
+			//(BooksAPI.search('Make'));
+			<div>
+					<input 
+						value={this.state.term}
+						onChange={this.handleChange} />
+						Value of input: {this.state.term}
+
+
+
+
+			</div>
+
+			);
+	}
+
+}
+
+export default ListBooks
+
